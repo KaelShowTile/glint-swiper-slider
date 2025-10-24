25395d27567079a8841ee626c8f8165d1814d813
KaelShowTile <kaelshowtile@gmail.com>
2025-10-24 11:02:41 +1100

    2025102401

--- includes/frontend.php
+++ includes/frontend.php
@@ -182,20 +182,16 @@ function glint_swiper_shortcode_handler($atts) {
                     
                     $output .= '<div class="swiper-slide product-slide">';
                     
-                    if ($product->is_on_sale()){
-                        $output .= '<span class="product-discount-rate">-' . round( (1-($sale_price/$regular_price)), 2)*100 . '%</span>';
-                    }
+                    
 
                     //for cht only
                     $sticker_url = get_field('product_icon_image', $product_id);
 
                     if($sticker_url){
-                        if($product->is_on_sale()){
-                            $output .= '<span class="product-sticker">';
-                        }else{
-                            $output .= '<span class="product-sticker no-sales">';
-                        }
-                        $output .= '<img src=' . $sticker_url . '></span>';
+                        $output .= '<span class="product-sticker"><img src=' . $sticker_url . '></span>';
+                    }
+                    elseif ($product->is_on_sale()){
+                        $output .= '<span class="product-discount-rate">-' . round( (1-($sale_price/$regular_price)), 2)*100 . '%</span>';
                     }
                     
                     $output .= '<a href="' . get_permalink() . '">';
